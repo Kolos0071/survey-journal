75db968c70f43bfe5e00974c7d8c4bdbd6aff8c2
Roman Ivanov <disaster0071@yandex.ru>
2025-12-08 02:58:56 +0500

    save file

--- lib/piquet_journal_screen/piquet_journal_screen.dart
+++ lib/piquet_journal_screen/piquet_journal_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:pickquet/model.dart';
+import 'package:path_provider/path_provider.dart';
 
 class PiquetJournalScreen extends StatelessWidget {
   PiquetJournalScreen({
@@ -20,11 +24,43 @@ class PiquetJournalScreen extends StatelessWidget {
     "down"
   ];
 
+  Future<File> get _localFile async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return File('${directory.path}/piquet_journal.th');
+  }
+
+  Future<void> writeMultipleRowsToFile(List<String> rows) async {
+    final file = await _localFile;
+    final content = rows.join('\n'); // Join the list of strings with newlines
+    await file.writeAsString(content,
+        mode: FileMode.write); // Overwrites if exists
+  }
+
+  void createFile() async {
+    List<String> fileString = [];
+
+    for (final item in measurementList) {
+      fileString.add(
+          "${item.from} ${item.to} ${item.distance} ${item.compass} ${item.angle} ${item.left} ${item.right} ${item.top} ${item.bottom}");
+      await writeMultipleRowsToFile(fileString);
+    }
+
+    final file = await _localFile;
+    String fileContent = await file.readAsString();
+    print(file.path);
+    print("File content:\n$fileContent");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Пикетажный журнал"),
+          title: const Text("Пикетажный журнал"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: createFile,
+          child: const Icon(Icons.save),
         ),
         body: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -34,39 +70,20 @@ class PiquetJournalScreen extends StatelessWidget {
               TableRow(children: [
                 for (final item in tableHeader) TableCell(child: Text(item))
               ]),
-              for (final row  in measurementList)
-              TableRow(children: [
-                TableCell(child: Text(row.from)),
-                TableCell(child: Text(row.to)),
-                TableCell(child: Text(row.distance.toString())),
-                TableCell(child: Text(row.compass.toString())),
-                TableCell(child: Text(row.angle.toString())),
-                TableCell(child: Text(row.left.toString())),
-                TableCell(child: Text(row.right.toString())),
-                TableCell(child: Text(row.top.toString())),
-                TableCell(child: Text(row.bottom.toString())),
-
-              ])
+              for (final row in measurementList)
+                TableRow(children: [
+                  TableCell(child: Text(row.from)),
+                  TableCell(child: Text(row.to)),
+                  TableCell(child: Text(row.distance.toString())),
+                  TableCell(child: Text(row.compass.toString())),
+                  TableCell(child: Text(row.angle.toString())),
+                  TableCell(child: Text(row.left.toString())),
+                  TableCell(child: Text(row.right.toString())),
+                  TableCell(child: Text(row.top.toString())),
+                  TableCell(child: Text(row.bottom.toString())),
+                ])
             ],
           ),
-        )
-        // Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: [
-        //     const Text("from to tape compass clino left right  up down"),
-        //     Expanded(
-        //       child: ListView.separated(
-        //           itemBuilder: (context, index) {
-        //             final item = measurementList[index];
-        //             return Text("${item.from} ${item.to} ${item.distance} ${item.compass} ${item.angle} ${item.left} ${item.right} ${item.top} ${item.bottom}");
-        //           },
-        //           separatorBuilder: (context, index) => SizedBox(
-        //                 height: 20,
-        //               ),
-        //           itemCount: measurementList.length),
-        //     ),
-        //   ],
-        // ),
-        );
+        ));
   }
 }
